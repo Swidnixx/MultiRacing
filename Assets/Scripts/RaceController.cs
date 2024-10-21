2994e602813834c44b71a9e8c214bf164c496356
Swidnixx <swidnixx@gmail.com>
2024-10-21 17:47:26 +0200

    Dźwięki i start i restart w UI

--- Assets/Scripts/RaceController.cs
+++ Assets/Scripts/RaceController.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class RaceController : MonoBehaviour
 {
     public static bool racePending = false;
     public static int totalLaps = 1;
     public int timer = 4;
+    public TMP_Text startText;
+    public AudioClip count;
+    public AudioClip start;
+    public GameObject endRacePanel;
 
     CheckpointController[] cars;
+    AudioSource audioSource;
 
     private void Start()
     {
+        endRacePanel.SetActive(false);
+        startText.gameObject.SetActive(false);
+
         InvokeRepeating(nameof(CountDown), 3, 1);
         cars = GameObject.FindObjectsOfType<CheckpointController>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void LateUpdate()
@@ -27,21 +38,39 @@ public class RaceController : MonoBehaviour
 
         if(finishers == cars.Length)
         {
-            Debug.Log("Race Finished!");
+            endRacePanel.SetActive(true);
             racePending = false;
         }
     }
 
     void CountDown()
     {
+        startText.gameObject.SetActive(true);
+
         timer--;
-        Debug.Log("Race starts in: " + timer);
+        startText.text = "Race starts in: " + timer;
 
         if(timer < 1)
         {
-            Debug.Log("Start!");
+            startText.text = "Start!";
+            audioSource.PlayOneShot(start);
             racePending = true;
             CancelInvoke();
+            Invoke(nameof(HideStartText), 1.6f);
         }
+        else
+        {
+            audioSource.PlayOneShot(count);
+        }
+    }
+
+    void HideStartText()
+    {
+        startText.gameObject.SetActive(false);
+    }
+
+    public void RestartRace()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
